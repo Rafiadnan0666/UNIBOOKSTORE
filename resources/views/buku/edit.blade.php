ae3096159b270b16af9f2830294dddd56d5a133e
Rafiadnan06666 <fn2344561@gmail.com>
2024-03-07 11:07:07 +0700

    oke

--- resources/views/buku/edit.blade.php
+++ resources/views/buku/edit.blade.php
@@ -1,52 +1,70 @@
 @extends('master.dash')
 @section('konten')
-    <form action="{{ route('penerbit.update', $penerbit->id) }}" enctype="multipart/form-data" method="post">
+    <form action="{{ route('buku.update', $buku->id) }}" enctype="multipart/form-data" method="post">
         @csrf
         @method('PUT')
         <div class="form-group">
             <div class="row">
                 <label for="kode" class="col-md-2">Kode</label>
-                <input type="text" value="{{ old('kode', $penerbit->kode) }}" name="kode"
-                       class="form-control mb-2 @error('kode') is-invalid @enderror col-md-6" id="kode">
+                <input type="text" value="{{ $buku->kode }}" name="kode"
+                    class="form-control mb-2 @error('kode') is-invalid @enderror col-md-6" id="kode">
                 @error('kode')
-                <p class="text-danger">{{ $message }}</p>
+                    <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="row">
-                <label for="tanggal" class="col-md-2">Tanggal</label>
-                <input type="datetime-local" value="{{ old('tanggal', $penerbit->tanggal) }}" name="tanggal"
-                       class="form-control mb-2 @error('tanggal') is-invalid @enderror col-md-6" id="tanggal">
-                @error('tanggal')
-                <p class="text-danger">{{ $message }}</p>
+                <label for="kategori" class="col-md-2">Kategori</label>
+                <input type="text" value="{{ $buku->kategori }}" name="kategori"
+                    class="form-control mb-2 @error('kategori') is-invalid @enderror col-md-6" id="kategori">
+                @error('kategori')
+                    <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="row">
-                <label for="nama" class="col-md-2">Nama</label>
-                <input type="text" value="{{ old('nama', $penerbit->nama) }}" name="nama"
-                       class="form-control mb-2 @error('nama') is-invalid @enderror col-md-6" id="nama" placeholder="Enter nama">
-                @error('nama')
-                <p class="text-danger">{{ $message }}</p>
+                <label for="nama_buku" class="col-md-2">Nama Buku</label>
+                <input type="text" value="{{ $buku->nama_buku }}" name="nama_buku"
+                    class="form-control mb-2 @error('nama_buku') is-invalid @enderror col-md-6" id="nama_buku"
+                    placeholder="Enter Nama Buku">
+                @error('nama_buku')
+                    <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="row">
-                <label for="alamat" class="col-md-2">Alamat</label>
-                <input type="text" value="{{ old('alamat', $penerbit->alamat) }}" name="alamat"
-                       class="form-control mb-2 @error('alamat') is-invalid @enderror col-md-6" id="alamat" placeholder="Enter alamat">
-                @error('alamat')
-                <p class="text-danger">{{ $message }}</p>
+                <label for="harga" class="col-md-2">Harga</label>
+                <input type="text" value="{{ $buku->harga }}" name="harga"
+                    class="form-control mb-2 @error('harga') is-invalid @enderror col-md-6" id="harga"
+                    placeholder="Enter Harga">
+                @error('harga')
+                    <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="row">
-                <label for="telepon" class="col-md-2">Telepon</label>
-                <input type="number" value="{{ old('telepon', $penerbit->telepon) }}" name="telepon"
-                       class="form-control mb-2 @error('telepon') is-invalid @enderror col-md-6" id="telepon" placeholder="Enter telepon">
-                @error('telepon')
-                <p class="text-danger">{{ $message }}</p>
+                <label for="stok" class="col-md-2">Stok</label>
+                <input type="number" value="{{ $buku->stok }}" name="stok"
+                    class="form-control mb-2 @error('stok') is-invalid @enderror col-md-6" id="stok"
+                    placeholder="Enter Stok">
+                @error('stok')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
+            </div>
+            <div class="row">
+                <label for="penerbit" class="col-md-2">Penerbit</label>
+                <select name="penerbit" id="penerbit"
+                    class="custom-select mb-2 @error('penerbit') is-invalid @enderror col-md-6">
+                    <option value="">-- Pilih Penerbit --</option>
+                    @foreach ($penerbit as $item)
+                        <option value="{{ $item->id }}" {{ $item->id == $buku->penerbiti->id ? 'selected' : '' }}>
+                            {{ $item->nama }}
+                        </option>
+                    @endforeach
+                </select>
+                @error('penerbit')
+                    <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
         </div>
         <button class="btn btn-primary mt-3" type="submit">
-            <i class="fas fa-save"></i> Update
+            <i class="fas fa-save"></i> Simpan
         </button>
     </form>
 @endsection
